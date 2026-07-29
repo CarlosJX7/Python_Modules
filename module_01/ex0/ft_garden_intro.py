d4ece8a5aca8e938d1faf105d09a231dc22f82f5
CarlosJX7 <carlos.jx7@gmail.com>
2026-07-29 19:40:26 +0200

    Update project

--- module_01/ex0/ft_garden_intro.py
+++ module_01/ex0/ft_garden_intro.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 def ft_garden_intro() -> None:
-    name = "Rose"
-    height = int(25)
-    age = int(30)
+    name: str = "Rose"
+    height: int = 25
+    age: int = 30
     print(f"""\
 === Welcome to My Garden ===
 Plant: {name}
