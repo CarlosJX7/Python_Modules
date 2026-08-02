74dc161f3d6cd7ab48afb45c6edd48224986181a
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-02 16:22:11 +0200

    Update ex4

--- module_01/ex4/ft_garden_security.py
+++ module_01/ex4/ft_garden_security.py
@@ -12,48 +12,67 @@ class Plant:
         self.name = name
         self.ratio = float(ratio)
         self.total = float(total)
-
-        self.set_heigth(height)
+        self._height = 0.0
+        self._age_days = 0
+        self.set_height(height)
         self.set_age(age_days)
 
-    def set_heigth(self, heigth: float) -> None:
-        heigth = float(heigth)
+    def set_height(self, height: float) -> bool:
+        heigth = float(height)
         if heigth < 0:
             print(f"{self.name.capitalize()}: ", end="")
             print("Error, height can't be negative")
-            print("Heigth update rejected")
-            return
+            print("Height update rejected")
+            return False
         else:
             self._height = float(heigth)
+            return True
 
-    def set_age(self, age: int) -> None:
-        age_days = float(age)
-        if age_days < 0:
+    def set_age(self, age: int) -> bool:
+        if age < 0:
             print(f"{self.name.capitalize()}: ", end="")
             print("Error, age can't be negative")
             print("Age update rejected")
-            return
+            return False
         else:
-            self._age_days = age_days
+            self._age_days = age
+            return True
 
-    def get_heigth (self) -> float:
+    def get_height(self) -> float:
         return self._height
 
-    def get_age(self) -> float:
+    def get_age(self) -> int:
         return self._age_days
 
     def show(self) -> None:
         name = self.name.capitalize()
-        print(f"{name}: {self.get_heigth()}cm, {self.get_age()} days old")
+        print(f"{name}: {self.get_height()}cm, {self.get_age()} days old")
 
     def grow(self) -> None:
-        self.height = round(self.height + self.ratio, 1)
+        self.set_height(round(self._height + self.ratio, 1))
         self.total += self.ratio
 
-    def age(self, days: int) -> None:
-        self.age_days += days
+    def age(self, days: int) -> bool:
+        if days < 0:
+            print(f"{self.name.capitalize()}: ", end="")
+            print("Error, age can't be negative")
+            print("Age update rejected")
+            return False
+        else:
+            self._age_days += days
+            return True
 
 
 if __name__ == "__main__":
-    plant1 = Plant("rose", -25, -30)
+    print("=== Garden Security System ===")
+    plant1 = Plant("rose", 15, 10)
+    print("Plant created: ", end="")
+    plant1.show()
+    if plant1.set_height(25):
+        print(f"\nHeight updated: {plant1.get_height()}cm")
+    if plant1.set_age(30):
+        print(f"Age updated: {plant1.get_age()} days\n")
+    plant1.set_age(-1)
+    plant1.set_height(-1)
+    print("\nCurrent state: ", end="")
     plant1.show()
