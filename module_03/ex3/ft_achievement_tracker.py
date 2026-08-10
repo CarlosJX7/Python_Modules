3f950629e877e3e7af87f08c00b50b311e990324
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-10 20:23:35 +0200

    Update ex3

--- module_03/ex3/ft_achievement_tracker.py
+++ module_03/ex3/ft_achievement_tracker.py
@@ -1,40 +1,88 @@
 import random
 
 
-def gen_player_achievements() -> set:
-    p_achievements = set()
+def gen_player_achievements() -> set[str]:
+    p_achievements: set[str] = set()
     random_size = random.randint(1, len(achievements))
     while len(p_achievements) < random_size:
-        random_index = random.randint(0, len(achievements)- 1)
-        p_achievements =  p_achievements.union({achievements[random_index]})
+        random_index = random.randint(0, len(achievements) - 1)
+        p_achievements = p_achievements.union({achievements[random_index]})
     return p_achievements
 
 
-def distinct(data: list[set]) -> set:
-        items = set()
-        for d in data:
-            items = items.union(d)
-        return items
+def distinct(data: list[set[str]]) -> set[str]:
+    items: set[str] = set()
+    for d in data:
+        items = items.union(d)
+    return items
 
 
-def intersection(data: list[set]) -> set:
+def intersection(data: list[set[str]]) -> set[str]:
     return set.intersection(*data)
 
 
-def unique(player_data: set, data: list[set]) -> set:
-    original_data = player_data
+def unique(player_name: str, players: dict[str, set[str]]) -> set[str]:
+    others: set[str] = set()
+    for name in players:
+        if name != player_name:
+            others = others.union(players[name])
+    return players[player_name].difference(others)
+
+
+def missing(player_data: set[str], data: list[str]) -> set[str]:
+    return set(data).difference(player_data)
+
+
+def get_example() -> dict[str, set[str]]:
+    players: dict[str, set[str]] = {
+        "Alice": {
+            "Crafting Genius",
+            "World Savior",
+            "Master Explorer",
+            "Collector Supreme",
+            "Untouchable",
+            "Boss Slayer",
+        },
+        "Bob": {
+            "Crafting Genius",
+            "Strategist",
+            "World Savior",
+            "Master Explorer",
+            "Unstoppable",
+            "Collector Supreme",
+            "Untouchable",
+        },
+        "Charlie": {
+            "Strategist",
+            "Speed Runner",
+            "Survivor",
+            "Master Explorer",
+            "Treasure Hunter",
+            "First Steps",
+            "Collector Supreme",
+            "Untouchable",
+            "Sharp Mind",
+        },
+        "Dylan": {
+            "Strategist",
+            "Speed Runner",
+            "Unstoppable",
+            "Untouchable",
+            "Boss Slayer",
+        },
+    }
+    return players
+
+
+def get_list(data: dict[str, set[str]]) -> list[set[str]]:
+    p_list: list[set[str]] = []
     for d in data:
-        if d == original_data:
-            continue
-        player_data = player_data.difference(d)
-    return player_data
+        p_list.append(data[d])
+    return p_list
 
-
-def missing(player_data: set, data: list[str]) -> set:
-    return set(achievements).difference(player_data)
 
 if __name__ == "__main__":
-    print("=== Achievement Tracker System ===")
+    print("=== Achievement Tracker System ===\n")
     achievements = [
                     'Crafting Genius',
                     'World Savior',
@@ -51,25 +99,22 @@ if __name__ == "__main__":
                     'Sharp Mind',
                     'Hidden Path Finder'
                     ]
-    players = {
-        "Alice": {'Crafting Genius', 'World Savior', 'Master Explorer', 'Collector Supreme', 'Untouchable', 'Boss Slayer'},
-        "Bob": {'Crafting Genius', 'Strategist', 'World Savior', 'Master Explorer', 'Unstoppable', 'Collector Supreme', 'Untouchable'},
-        "Charlie": {'Strategist', 'Speed Runner', 'Survivor', 'Master Explorer', 'Treasure Hunter', 'FirstSteps', 'Collector Supreme', 'Untouchable', 'Sharp Mind'},
-        "Dylan": {'Strategist', 'Speed Runner', 'Unstoppable', 'Untouchable', 'Boss Slayer'}
-        }
-    list_sets = []
-    for name in players:
-        print(f"Player: {name}", players[name])
-        list_sets.append(set(players[name]))
-    for sets in list_sets:
-        print(f"sets: {sets}")
+    players = get_example()
+    # players = {
+    #             "Alice" : gen_player_achievements(),
+    #             "Bob" : gen_player_achievements(),
+    #             "Charlie" : gen_player_achievements(),
+    #             "Dylan" : gen_player_achievements()
+    #             }
 
-    result = distinct(list_sets)
-    print(f"union {result}")
-    print(f"intersection {intersection(list_sets)}")
-    print(f"difference: {unique(players['Alice'], list_sets)}")
-    print(f"difference charlie: {unique(players['Charlie'], list_sets)}")
-    print(f"missing alice: {missing(players['Alice'], achievements)}")
-    print(f"missing bob {missing(players['Bob'], achievements)}")
-    print(f"missing charlie {missing(players['Charlie'], achievements)}")
-    print(f"missing dylan {missing(players['Dylan'], achievements)}")
+    for p in players:
+        print(f"Player {p}: {players[p]}")
+    list_sets = get_list(players)
+    print(f"\nAll distinct achivements: {distinct(list_sets)}")
+    print(f"\nCommon achievements: {intersection(list_sets)}\n")
+
+    for p in players:
+        print(f"Only {p} has {unique(p, players)}")
+    print()
+    for p in players:
+        print(f"{p} is missing: {missing(players[p], achievements)}")
