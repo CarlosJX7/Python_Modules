5981b36919b1f44c1923817f44951514ca4139ab
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-11 21:06:36 +0200

    Update ex1

---
+++ module_04/ex1/ft_archive_creation.py
@@ -0,0 +1,67 @@
+import sys
+import typing
+
+
+def save_text(text: str) -> None:
+    file_name = input("Enter new file name (or empty): ")
+    if file_name == "":
+        print("Not saving data.")
+        return
+    print(f"Saving data to: {file_name}")
+    try:
+        new_file = open(file_name, "w")
+        new_file.write(text)
+        new_file.close()
+        print(f"Data saved in {file_name}")
+    except OSError as e:
+        print(f"Error creating file '{file_name}': {e}")
+
+def format_text(text: str) -> str:
+    new_text = ""
+
+    for char in text:
+        if char == "\n":
+            char = "#\n"
+        new_text = new_text + char
+
+    if new_text != "" and new_text[-1] != "\n":
+        new_text = new_text + "#"
+
+    return new_text
+
+
+def get_content(fd: typing.IO[str]) -> str:
+    text = fd.read()
+    return text
+
+
+def data_recover() -> str:
+    file = sys.argv[1]
+    try:
+        print("=== Cyber Archives Recovery ===")
+        print(f"Accessing file '{file}'")
+        fd = open(file)
+        print("---")
+        new_text = get_content(fd)
+        print(new_text, end="")
+        fd.close()
+        print(f"\n---\nFile '{file}' closed.")
+        return new_text
+    except OSError as e:
+        print(f"Error opening file '{file}': {e}")
+        return ""
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+    text = data_recover()
+    print("Transform data: \n---")
+    new_text = format_text(text)
+    print(new_text, "\n---")
+    save_text(new_text)
+
+
+if __name__ == "__main__":
+    main()
