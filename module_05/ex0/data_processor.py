d413ea56cb2eb9ed6cd98dc982c78304038da547
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-17 16:19:55 +0200

    Update ex0

--- module_05/ex0/data_processor.py
+++ module_05/ex0/data_processor.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
     def __init__(self) -> None:
         self._data_list: list[str] = []
@@ -10,13 +11,13 @@ class DataProcessor(ABC):
     def validate(self, data: Any) -> bool:
         pass
 
-
     @abstractmethod
     def ingest(self, data: Any) -> None:
         pass
 
-
     def output(self) -> tuple[int, str]:
+        if not self._data_list:
+            raise ValueError("No data to output")
         output_data = self._data_list.pop(0)
         output_index = self._index
         self._index += 1
@@ -30,22 +31,20 @@ class NumericProcessor(DataProcessor):
                 if not isinstance(d, (int, float)):
                     return False
 
-        elif not isinstance(data, (int, float)) :
+        elif not isinstance(data, (int, float)):
             return False
 
         return True
 
-
     def ingest(self, data: int | float | list[int | float]) -> None:
         if not self.validate(data):
             raise TypeError("Error en ingest de numeric_processor")
-
-        if isinstance(data, list):
-            for element in data:
-                self._data_list.append(str(element))
-
+        if not isinstance(data, list):
+            data_list = [data]
         else:
-            self._data_list.append(str(data))
+            data_list = data
+        for element in data_list:
+            self._data_list.append(str(element))
 
 
 class TextProcessor(DataProcessor):
@@ -60,41 +59,48 @@ class TextProcessor(DataProcessor):
 
         return True
 
-
     def ingest(self, data: str | list[str]) -> None:
         if not self.validate(data):
             raise ValueError("Error en inges de text processor")
 
-        if isinstance(data, list):
-            for string in data:
-                self._data_list.append(string)
+        if not isinstance(data, list):
+            data_list = [data]
         else:
-            self._data_list.append(data)
+            data_list = data
+        for string in data_list:
+            self._data_list.append(string)
 
 
 class LogProcessor(DataProcessor):
     def validate(self, data: Any) -> bool:
+        def validate_dict(d: Any) -> bool:
+            if not isinstance(d, dict):
+                return False
+            for key, value in d.items():
+                if not isinstance(key, str) or not isinstance(value, str):
+                    return False
+            return True
         if isinstance(data, list):
             for element in data:
-                if not isinstance(element, dict):
+                if not validate_dict(element):
                     return False
+            return True
 
         if not isinstance(data, dict):
             return False
 
         return True
 
-
-    def ingest(self, data: dict | list[dict]) -> None:
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
         if not self.validate(data):
             raise ValueError("Error en ingest de Log")
 
-        if isinstance(data, list):
-            for element in data:
-                for string in element.values():
-                    self._data_list.append(string)
+        if not isinstance(data, list):
+            list_data = [data]
+        else:
+            list_data = data
 
-        if isinstance(data, dict):
-            for string in data.values():
-                self._data_list.append(string)
-
+        for element in list_data:
+            level = element.get("log_level", "default_level")
+            message = element.get("log_message", "default_message")
+            self._data_list.append(f"{level}:{message}")
