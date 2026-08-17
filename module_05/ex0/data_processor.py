ac1db9c0873063d8de91dd668f714091004368a1
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-17 18:19:56 +0200

    Add main func to ex0

--- module_05/ex0/data_processor.py
+++ module_05/ex0/data_processor.py
@@ -38,7 +38,7 @@ class NumericProcessor(DataProcessor):
 
     def ingest(self, data: int | float | list[int | float]) -> None:
         if not self.validate(data):
-            raise TypeError("Error en ingest de numeric_processor")
+            raise ValueError("Error en ingest de numeric_processor")
         if not isinstance(data, list):
             data_list = [data]
         else:
@@ -103,4 +103,78 @@ class LogProcessor(DataProcessor):
         for element in list_data:
             level = element.get("log_level", "default_level")
             message = element.get("log_message", "default_message")
-            self._data_list.append(f"{level}:{message}")
+            self._data_list.append(f"{level}: {message}")
+
+
+def test_log() -> None:
+    print("\nTesting Log Processor...")
+    data_list = ["Hello", [{"log_level": "NOTICE", "log_message": "Connection to server"},
+                        {"log_level": "ERROR", "log_message": "Unauthorized access!!"}]]
+    processor = LogProcessor()
+    print(f"Trying to validate input '{data_list[0]}: '"
+        f"{processor.validate(data_list[0])}")
+    print(f"Processing data: {data_list[1]}")
+    processor.ingest(data_list[1])
+    size = 2
+    print(f"Extracting {size} values...")
+    for i in range(size):
+        try:
+            extracted = processor.output()
+        except ValueError as e:
+            print(f"Got exception: {e}")
+            break
+        else:
+            print(f"Log entry {extracted[0]}: {extracted[1]}")
+
+
+def test_text() -> None:
+    print("\nTesting Text Processor...")
+    data_list = [42, ["Hello", "Nexus", "World"]]
+    processor = TextProcessor()
+    print(f"Trying to validate input '{data_list[0]}':"
+        f"{processor.validate(data_list[0])}")
+    print(f"Processing data: {data_list[-1]}")
+    processor.ingest(data_list[-1])
+    size = 1
+    print(f"Extracting {size} value...")
+    for i in range(size):
+        try:
+            extracted = processor.output()
+        except ValueError as e:
+            print(f"Got exception: {e}")
+            break
+        else:
+            print(f"Text value {extracted[0]}: {extracted[1]}")
+
+def test_numeric() -> None:
+    print("Testing numeric processor...")
+    data_list = [42, "Hello", "foo", [1, 2, 3, 4, 5]]
+    processor = NumericProcessor()
+    for data in data_list[:2]:
+        print(f"Trying to validate '{data}': {processor.validate(data)}")
+    print(f"Test invalid ingestion of string '{data_list[2]}' without prior validation:")
+    try:
+        processor.ingest(data_list[2])
+    except ValueError as e:
+        print(f"Got exception: {e}")
+
+    print(f"Processing data: {data_list[-1]}")
+    processor.validate(data_list[-1])
+    processor.ingest(data_list[-1])
+    size = 3
+    print(f"Extracting {size} values...")
+    for i in range(0, size):
+        try:
+            extracted = processor.output()
+        except ValueError as e:
+            print(f"Got exception: {e}")
+            break
+        else:
+            print(f"Numeric value: {extracted[0]}: {extracted[1]}")
+
+
+if __name__ == "__main__":
+    print("=== Code Nexus - Data Processor ===")
+    test_numeric()
+    test_text()
+    test_log()
