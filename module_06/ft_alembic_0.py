0991b66bbee8af2203cf74fbb988c4b20ef3239f
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-19 17:47:42 +0200

    Update project

---
+++ module_06/ft_alembic_0.py
@@ -0,0 +1,4 @@
+from elements import create_fire
+
+print("Using: 'import ...' structure to access elements.py")
+print(f"Testing create_fire: {create_fire()}")
---
+++ module_06/ft_alembic_2.py
@@ -0,0 +1,4 @@
+from alchemy import create_earth
+
+print("Accessing alchemy/elements.py using 'import ...' structure")
+print(f"Testing create_earth: {create_earth()}")
