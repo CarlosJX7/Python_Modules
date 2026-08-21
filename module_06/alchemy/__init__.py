6c26ba291137912fa65ff52c95e211a5f187b8ac
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-21 16:57:59 +0200

    Update part II

--- module_06/alchemy/__init__.py
+++ module_06/alchemy/__init__.py
@@ -1,5 +1,5 @@
 from .elements import create_air
 from .potions import strength_potion
+from .potions import healing_potion
 
-__all__ = ["create_air"]
-__all__ = ["strength_potion"]
+__all__ = ["create_air", "strength_potion", "healing_potion"]
