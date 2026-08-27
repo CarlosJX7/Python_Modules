182811596c786210f3473aad9d3eecaff3855cfb
CarlosJX7 <carlos.jx7@gmail.com>
2026-08-27 13:14:10 +0200

    Update ex0

--- module_08/ex0/construct.py
+++ module_08/ex0/construct.py
@@ -1,3 +1,18 @@
-import sys
 import os
+import sys
 import site
+
+def inside_env() -> bool:
+    return sys.prefix != getattr(sys, "base_prefix", sys.prefix)
+
+if __name__ == "__main__":
+    if not inside_env():
+        print("MATRIX STATUS: You're still plugged in")
+        print(f"Current Python: {sys.executable}")
+        print("Virtual Environment: None detected")
+    else:
+        env_name = os.path.basename(sys.prefix)
+        print("MATRIX STATUS: Welcome to the construct")
+        print(f"Current Python: {sys.executable}")
+        print(f"Virtual Environment: {env_name}")
+        print(site.getsitepackages()[0])
