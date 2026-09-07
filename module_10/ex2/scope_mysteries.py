5fe3f298313d5e36ef07fe3db9c52eae251a7e40
CarlosJX7 <carlos.jx7@gmail.com>
2026-09-07 17:31:34 +0200

    Add ex2

---
+++ module_10/ex2/scope_mysteries.py
@@ -0,0 +1,20 @@
+from collections.abc import Callable
+
+
+def mage_counter() -> Callable[[], int]:
+    total_calls = 0
+    def func_counter() -> int:
+        nonlocal total_calls
+        total_calls += 1
+        return total_calls
+    return func_counter
+
+
+if __name__ == "__main__":
+    func1 = mage_counter()
+    func2 = mage_counter()
+
+    print(f"Function 1 called: {func1()}")
+    print(f"Function 2 called: {func2()}")
+    print(f"Function 2 called: {func2()}")
+    print(f"Function 1 called: {func1()}")
