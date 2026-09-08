8e8abb7110b02bff2bf8c8bcdf49011bd33d2190
CarlosJX7 <carlos.jx7@gmail.com>
2026-09-08 17:06:02 +0200

    Update ex2

--- module_10/ex2/scope_mysteries.py
+++ module_10/ex2/scope_mysteries.py
@@ -10,6 +10,14 @@ def mage_counter() -> Callable[[], int]:
         return total_calls
     return func_counter
 
+def spell_accumulator(initial_power: int)-> Callable[[int], int]:
+    power = initial_power
+    def accumulator(amount: int) -> int:
+        nonlocal power
+        power += amount
+        return power
+    return accumulator
+
 
 if __name__ == "__main__":
     func1 = mage_counter()
@@ -19,3 +27,11 @@ if __name__ == "__main__":
     print(f"Function 2 called: {func2()}")
     print(f"Function 2 called: {func2()}")
     print(f"Function 1 called: {func1()}")
+
+    func1 = spell_accumulator(20)
+    func2 = spell_accumulator(20)
+
+    print(f"Function 1 called: {func1(1)}")
+    print(f"Function 2 called: {func2(1)}")
+    print(f"Function 2 called: {func2(10)}")
+    print(f"Function 1 called: {func1(10)}")
