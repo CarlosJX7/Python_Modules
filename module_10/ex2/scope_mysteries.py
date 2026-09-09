c5c3724c9cb8b9728f4f604d60141cec03221d0e
CarlosJX7 <carlos.jx7@gmail.com>
2026-09-09 21:39:15 +0200

    Update ex2

--- module_10/ex2/scope_mysteries.py
+++ module_10/ex2/scope_mysteries.py
@@ -19,6 +19,21 @@ def spell_accumulator(initial_power: int)-> Callable[[int], int]:
     return accumulator
 
 
+def enchantment_factory(enchantment_type: str) -> Callable[[str], str]:
+    def add_enchantment(item_name: str) -> str:
+        return f"{enchantment_type} {item_name}"
+    return add_enchantment
+
+
+def memory_vault() -> dict[str, Callable]:
+    vault: dict[str, int] = {}
+    def store(key: str, value: int):
+        vault[key] = value
+    def recall(key: str) -> int:
+        return vault[key]
+    return {"store": store, "recall": recall}
+
+
 if __name__ == "__main__":
     func1 = mage_counter()
     func2 = mage_counter()
@@ -35,3 +50,13 @@ if __name__ == "__main__":
     print(f"Function 2 called: {func2(1)}")
     print(f"Function 2 called: {func2(10)}")
     print(f"Function 1 called: {func1(10)}")
+
+    ench = enchantment_factory("flaming")
+    data = ench("sword")
+    print(data)
+
+    vault = memory_vault()
+    store_func = vault["store"]
+    store_func("secret", 42)
+    recall_func = vault["recall"]
+    print(f"{recall_func('secret')}")
