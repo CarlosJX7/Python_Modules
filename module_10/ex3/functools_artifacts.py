3d58301e3777a17bc0d225a3de019ba4c0a37980
CarlosJX7 <carlos.jx7@gmail.com>
2026-09-09 21:48:13 +0200

    Update ex3

---
+++ module_10/ex3/functools_artifacts.py
@@ -0,0 +1,18 @@
+def spell_reducer(spells: list[int], operation: str) -> int:
+    pass
+
+
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+    pass
+
+
+def memoized_fibonacci(n: int) -> int:
+    pass
+
+
+def spell_dispatcher() -> Callable[[Any], str]:
+    pass
+
+
+if __name__ == "__main__":
+    pass
